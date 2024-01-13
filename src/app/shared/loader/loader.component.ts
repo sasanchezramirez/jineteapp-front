d4c3cf396624faf1a5f5d5d947615cc50c0661a1
santiago <sasanchezramirez@gmail.com>
2024-01-12 22:35:14 -0500

    feature: UX for login

---
+++ src/app/shared/loader/loader.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-loader',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss'
+})
+export class LoaderComponent {
+  @Input() isLoading: boolean = false;
+
+}
