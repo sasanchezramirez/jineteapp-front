f801a6a40676dd9cdc277c6192bbe2cda166e1e5
santiago <sasanchezramirez@gmail.com>
2024-02-03 21:03:24 -0500

    feature: great progress with the new ui on dashboard

--- src/app/shared/boards/calendar-card/calendar-card.component.ts
+++ src/app/shared/boards/calendar-card/calendar-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-calendar-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './calendar-card.component.html',
   styleUrl: './calendar-card.component.scss'
 })
 export class CalendarCardComponent {
-
+@Input() currentMonth: Date = new Date;
+@Input() dayBackgroundColor: number = 0;
+@Input() weekDays: any[] = [];
+@Input() daysInMonth: any[] = [];
 }
