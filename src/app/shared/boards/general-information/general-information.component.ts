e0386ba2d3d756e4638c2dcd336d1e5d21f2d311
santiago <sasanchezramirez@gmail.com>
2024-03-08 19:37:37 -0500

    feature: improving general description

--- src/app/shared/boards/general-information/general-information.component.ts
+++ src/app/shared/boards/general-information/general-information.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-general-information',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './general-information.component.scss'
 })
 export class GeneralInformationComponent {
+  @Input() cardName: string = '';
 
 }
